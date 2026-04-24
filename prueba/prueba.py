954e7b1abb5930e8273da19b8c9894eed29e33ad
Dylan Gerardo Garbanzo Fallas <dgarbanzof@estudiantec.cr>
2026-04-23 18:37:01 -0600

    Add prueba.py with area calculation and division

---
+++ prueba/prueba.py
@@ -0,0 +1,18 @@
+# linprod.py
+
+def calcular_area(base,altura):
+    area=base*altura
+    return area
+
+def dividir(a,b):
+    return a/b
+
+numeros=[1,2,3,4,5]
+for i in range(len(numeros)):
+    print(numeros[i])
+
+x = 10
+y = 20
+z = x+y
+resultado=calcular_area(5,3)
+print(resultado)
